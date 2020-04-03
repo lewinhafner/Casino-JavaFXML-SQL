273b7ae93b6419078fca8ec226f5c9bc5ecf28b6
Mischa <mischa.fischer4@bbbaden.ch>
2020-04-03 14:25:25 +0200

    überabeitete Hilfe

--- src/ch/bbbaden/casino/videopoker/VideoPokerHilfeController.java
+++ src/ch/bbbaden/casino/videopoker/VideoPokerHilfeController.java
@@ -32,10 +32,13 @@ public class VideoPokerHilfeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        hilfeTxt.setText("1. über den Knopf zwischen den Bet köpfen value des Chipes auswählen" + "\n" + "2. über die Betknöpfe auswählen wie viele Coins gesetzt werdem"
+        hilfeTxt.setText("1. über den Knopf zwischen den Bet köpfen value des Chipes auswählen" + "\n" + "2. über die Betknöpfe auswählen wie viele Coins gesetzt werden"
                 + "\n" + "3. Auf deal klicken" + "\n" + "4. Auf die Karten klicken, die man wegwerfen will." +"\n"+ "5. Auf Draw klicken" + 
                 "\n" + "6. wenn gewonnen auf Gamble klicken will, wenn man den Gewinn verwetten will"
-                +"\n"+"7. im Gamblemodus auf eine Karte klicken, die noch nicht aufgedeckt wurde" + "\n"
+                +"\n"+"7. im Gamblemodus auf eine Karte klicken, die noch nicht aufgedeckt wurde" + "\n"+
+                "natural_Royal_Flush -> A,K,Q,J,10(gleiche Farbe) || vier_deuces -> 4 * 2 ||Royal_Flush -> A,K,Q,J,10 (gleiche Farbe + Joker)"+"\n"+"5ofAKind -> 5* gleiches Zeichen ||"
+                + " straight_Flush -> 5er Folge gleiche Farben(Joker erlaubt, nicht mit Ass beginnend)"+"\n"+"4ofAKind -> 4*gleiches Zeichen(Joker) || FullHouse -> 3 und 2 * gleiche Zeichen(Joker)"
+                +"\n" + "flush -> 5* selbe Farbe || straight -> 5er Folge(Joker) || 3ofAKind -> 3*gleichesZeichen"
         );
         
     }    
